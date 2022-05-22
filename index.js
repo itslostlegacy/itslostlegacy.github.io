25079b1871cf22a1fecc210467a3c124903dd456
unknown <nineplustenisnot21@gmail.com>
2022-05-22 15:50:53 -0400

    e

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let i = 0;
 let title = 'ItsLostLegacy';
-let speed = 100; 
+let speed = 100;
 
 function typeWriter() {
   if (i < title.length) {
@@ -8,18 +8,4 @@ function typeWriter() {
     i++;
     setTimeout(typeWriter, speed);
   }
-}
-
-async function secretFunction() {
-  var height = document.body.clientHeight - 200;
-  var width = document.body.clientWidth - 200;
-  var b = document.querySelector("button");
-  b.addEventListener("click", secretFunction);
-  
-  var i = Math.floor(Math.random() * (width - 0) + 0);
-  var j = Math.floor(Math.random() * (height - 0) + 0)
-  b.style.left = i+"px";
-  b.style.top = j+"px";
-  b.style.display = "block";
-  b.style.position = "absolute";
 }
